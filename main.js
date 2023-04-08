fbbbaae035603efa0a37b3fd6de37a62cde300ea
itsCoolguy <cadanpil@gmail.com>
2023-04-08 21:45:09 +0100

    update

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-$("#slider").on("input change", (e)=>{
+function moveSlider(e) {
+    //console.log(e)
     const sliderPos = e.target.value;
     // Update the width of the foreground image
+    //console.log('hi')
     $('#before-image').css('width', `${sliderPos}%`)
     // Update the position of the slider button
     $('.slider-button').css('left', `calc(${sliderPos}% - 18px)`)
+}
+
+$("#slider").on("input change", (e)=>{
+    moveSlider(e);
 });
+
+document.querySelector('#slider').addEventListener('touchstart', (e)=>{
+    moveSlider(e);
+})
+document.querySelector('#slider').addEventListener('touchmove', (e)=>{
+    moveSlider(e);
+})
+document.querySelector('#slider').addEventListener('touchend', (e)=>{
+    moveSlider(e);
+})
